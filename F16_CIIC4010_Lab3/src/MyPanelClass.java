dd691fc4e6f521f1580ca1e44cb17e5c87c01cbb
4010d4 <4010d4@quenepa.ece.uprm.edu>
2016-09-01 16:12:12 -0400

    Drew the diagonal lines (then, they were commented) and drew the center oval.

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -26,12 +26,24 @@ public class MyPanelClass extends JPanel {
                         g.setColor(Color.GRAY);
                         g.fillRect(x1, y1, width+1, height+1);
                         
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width, height);
+//                        //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        //Draw a inner border
+//                        g.setColor(Color.BLACK);
+//                        g.drawRect(x1+3, y1+3, width-6, height-6);
+//                        
+//                        //Diagonals from top left to bottom line
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        //Diagonals from top right to bottom left
+//                        g.setColor(Color.RED);
+//                        g.drawLine(x2, y1, x1, y2);
                         
-                        //Draw a inner border
-                        g.setColor(Color.BLACK);
-                        g.drawRect(x1+3, y1+3, width-6, height-6);
+                        //The oval on the center
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval((width/2)-27, (height/2)-27, 55, 55);
             }
 }
